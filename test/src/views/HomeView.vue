ef9beb881966b05e9103cbe6d754727ff7669b18
dingyuanjie <911260800@qq.com>
2023-07-07 08:48:46 +0800

    启动压缩图片

--- test/src/views/HomeView.vue
+++ test/src/views/HomeView.vue
@@ -1,4 +1,6 @@
+
 <script setup lang="ts">
+// @ts-nocheck
 import { reactive, watch,ref } from 'vue';
 import axios from 'axios';
 import { ElMessage } from 'element-plus'
--- test/vite.config.ts
+++ test/vite.config.ts
@@ -3,12 +3,43 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import viteCompression from 'vite-plugin-compression'
+import viteImagemin from 'vite-plugin-imagemin'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    viteCompression({
+      threshold: 10
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 20
+      },
+      pngquant: {
+        quality: [0.2, 0.3],
+        speed: 4
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
